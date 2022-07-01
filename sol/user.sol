7b9b884adf1b88a5c13b78e048c18112302157f2
etsraphael <ets_raphael@outlook.fr>
2022-07-01 19:55:27 +1000

    User.json added

---
+++ sol/user.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract User {
+   
+    struct Member {
+        address owner;
+        string userName;
+        bool isValid;
+    }
+
+    Member[] private memberList;
+    mapping (address => Member) private members;
+
+    function setAccount(string memory _userName) public returns (bool) {
+        members[msg.sender] = Member(address(msg.sender), _userName, true);
+        return true;
+    }
+
+    function deleteMyAccount() public returns (bool) {
+        members[msg.sender] = Member(address(0), "", false);
+        return true;
+    }
+
+    function getAccount() public view returns (Member memory) {
+        if(members[msg.sender].owner == msg.sender) return members[msg.sender];
+        else return Member(address(0), "", false);
+    }
+
+}
